33197b013f3c643d6c6dada41470459d2f90b4c0
LynithDev <61880709+LynithDev@users.noreply.github.com>
2023-10-15 22:02:04 +0200

    Add nesting

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -1,7 +1,19 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+// @ts-ignore - Tailwind doesn't have types for this yet
+import postcssNesting from "tailwindcss/nesting";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()]
+	integrations: [tailwind()],
+	vite: {
+		css: {
+			postcss: {
+				plugins: [
+					postcssNesting,
+				]
+			}
+		}
+	}
 });
