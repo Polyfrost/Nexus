d6c7fad43961975026a7e4e9ae21406f505e0a2e
LynithDev <61880709+LynithDev@users.noreply.github.com>
2023-12-28 16:34:52 +0100

    fullpage.js

---
+++ apps/website/public/scripts/scroll.js
@@ -0,0 +1,15 @@
+/* eslint-disable new-cap */
+/* eslint-disable no-new */
+new fullpage('#fullpage', {
+	licenseKey: 'gplv3-license',
+	autoScrolling: true,
+	scrollBar: true,
+	scrollHorizontally: false,
+	fixedElements: '',
+	fitToSection: false,
+	// navigation: true,
+	// navigationPosition: 'right',
+	credits: {
+		enabled: false,
+	},
+});
