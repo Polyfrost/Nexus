1e11b59b2910a7320a3cf8c656913fb07f317684
Pauline <git@ethanlibs.co>
2024-01-22 09:57:53 +0100

    start website refactor (will do the rest later )

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -1,13 +1,21 @@
+import process from 'node:process';
+
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 import { defineConfig } from 'astro/config';
 import postcssNesting from 'tailwindcss/nesting';
 
+const VERCEL_PREVIEW_SITE
+	= process.env.VERCEL_ENV !== 'production'
+	&& process.env.VERCEL_URL
+	&& `https://${process.env.VERCEL_URL}`;
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://polyfrost.org',
+	site: VERCEL_PREVIEW_SITE || 'https://polyfrost.org',
 	integrations: [
 		tailwind(),
 		mdx(),
@@ -19,5 +27,12 @@ export default defineConfig({
 				plugins: [postcssNesting()],
 			},
 		},
+		ssr: {
+			noExternal: ['smartypants'],
+		},
 	},
+	output: 'hybrid',
+	adapter: vercel({
+		functionPerRoute: false,
+	}),
 });
