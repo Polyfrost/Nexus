8aed1203b791c37ab82658ff4ff84cd9c1b51bd4
Pauline <git@ethanlibs.co>
2024-05-12 00:17:49 +0200

    🐛 fix(website): fix scrolling

--- apps/website/src/pages/api/releases/oneconfig/loader/[version].ts
+++ apps/website/src/pages/api/releases/oneconfig/loader/[version].ts
@@ -1,3 +1,3 @@
-import type { APIRoute } from 'astro';
+// import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async ({ request, params, redirect }) => {};
+// export const GET: APIRoute = async ({ request, params, redirect }) => {};
