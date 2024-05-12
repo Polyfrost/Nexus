8375afc6bac403abfae114cbd95f621c5b37e980
Pauline <git@ethanlibs.co>
2024-05-12 04:25:37 +0200

    🚧 wip(website): dsdjffjf

--- apps/website/src/pages/api/releases/oneconfig/loader/[version].ts
+++ apps/website/src/pages/api/releases/oneconfig/loader/[version].ts
@@ -1,3 +1,5 @@
-// import type { APIRoute } from 'astro';
+import type { APIRoute } from 'astro';
 
-// export const GET: APIRoute = async ({ request, params, redirect }) => {};
+export const GET: APIRoute = async () => {
+	return Response.json({ message: 'hi there' }, { status: 301 });
+};
